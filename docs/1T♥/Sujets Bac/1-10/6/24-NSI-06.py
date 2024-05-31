4d2f0cd59ff1e1c67d3a43f40ec137ff8cc20545
Tendus <107099111+Tenduus@users.noreply.github.com>
2024-05-31 07:49:17 +0000

    a

--- docs/1T♥/Sujets Bac/1-10/6/24-NSI-06.py
+++ docs/1T♥/Sujets Bac/1-10/6/24-NSI-06.py
@@ -20,3 +20,8 @@ def vainqueurs(election):
     return ... 
 
 
+-Mémorisation des résultats intermédiaires : Le dictionnaire delannoy_mem est utilisé pour stocker les résultats des appels récursifs déjà calculés.
+-Condition de base : Si n ou m est nul (n == 0 ou m == 0), il n'y a qu'un seul chemin possible (en ligne droite), donc le résultat est 1.
+-Cas général : Si ni n ni m ne sont nuls, le nombre de chemins allant de (0, 0) à (n, m) est la somme des chemins provenant de (n-1, m), (n, m-1) et (n-1, m-1).
+-Mémorisation du résultat : Le résultat est mémorisé dans delannoy_mem pour éviter des calculs redondants futurs.
+-Retour du résultat mémorisé : La fonction retourne le résultat mémorisé.
